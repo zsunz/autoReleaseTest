0e0988c18b71e4115db4ae80da9dcf82c7aeacd6
zsunz <komlov@naver.com>
2024-07-21 22:18:11 +0900

    ㅇㅇㅇㅇㅇ

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app/step1/autoReleaseTest"
+PROJECT_ROOT="/home/ubuntu/app/autoReleaseTest"
 JAR_FILE="$PROJECT_ROOT/build/libs/autoReleaseTest-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
