627b621a296e43595383a5c0ff6761441534e565
zsunz <komlov@naver.com>
2024-07-22 17:02:53 +0900

    스프링 재시작555

--- start.sh
+++ start.sh
@@ -9,7 +9,6 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
@@ -21,4 +20,4 @@ nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
